3679ae153e47bc1d7ce2681fd864ebffb8669da3
Andreas Schacht <schacht@kaliber5.de>
2016-12-06 12:41:59 +0000

    remove voters extending from removed AbstractVoter

--- SyliusSecurityBundle/Voter/AbstractApiVoter.php
+++ SyliusSecurityBundle/Voter/AbstractApiVoter.php
@@ -3,10 +3,14 @@ namespace Kaliber5\SyliusSecurityBundle\Voter;
 
 use Kaliber5\LoggerBundle\LoggingTrait\LoggingTrait;
 use Sylius\Component\Resource\ResourceActions;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
 
 /**
+ * @TODO this class was extending the AbstractVoter, removed in sf3.2, and should maybe refactored
+ *
  * Class AbstractApiVoter
  *
  * This is an abstract class that supports all
@@ -14,7 +18,7 @@ use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
  *
  * @package Kaliber5\SyliusSecurityBundle\Voter
  */
-abstract class AbstractApiVoter extends AbstractVoter implements VoterInterface
+abstract class AbstractApiVoter extends Voter implements VoterInterface
 {
     use LoggingTrait;
 
@@ -25,12 +29,80 @@ abstract class AbstractApiVoter extends AbstractVoter implements VoterInterface
      */
     protected function getSupportedAttributes()
     {
-        return array(
+        return [
             ResourceActions::CREATE,
             ResourceActions::DELETE,
             ResourceActions::INDEX,
             ResourceActions::SHOW,
-            ResourceActions::UPDATE
-        );
+            ResourceActions::UPDATE,
+        ];
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function supports($attribute, $subject)
+    {
+        if ($this->supportsClass(get_class($subject))) {
+            return $this->supportsAttribute($attribute);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        return $this->isGranted($attribute, $subject, $token->getUser());
+    }
+
+    /**
+     * This is from the sf2.8 AbstractVoter, removed in sf3.2, maybe this should be refactored
+     */
+    protected function supportsAttribute($attribute)
+    {
+        return in_array($attribute, $this->getSupportedAttributes());
+    }
+
+    /**
+     * This is from the sf2.8 AbstractVoter, removed in sf3.2, maybe this should be refactored
+     */
+    protected function supportsClass($class)
+    {
+        foreach ($this->getSupportedClasses() as $supportedClass) {
+            if ($supportedClass === $class || is_subclass_of($class, $supportedClass)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * This is from the sf2.8 AbstractVoter, to aware compatibility. removed in sf3.2, maybe this should be refactored
+     *
+     * Perform a single access check operation on a given attribute, object and (optionally) user
+     * It is safe to assume that $attribute and $object's class pass supportsAttribute/supportsClass
+     * $user can be one of the following:
+     *   a UserInterface object (fully authenticated user)
+     *   a string               (anonymously authenticated user).
+     *
+     * @param string               $attribute
+     * @param object               $object
+     * @param UserInterface|string $user
+     *
+     * @return bool
+     */
+    abstract protected function isGranted($attribute, $object, $user = null);
+
+    /**
+     * This is from the sf2.8 AbstractVoter, to aware compatibility. removed in sf3.2, maybe this should be refactored
+     * 
+     * Return an array of supported classes. This will be called by supportsClass.
+     *
+     * @return array an array of supported classes, i.e. array('Acme\DemoBundle\Model\Product')
+     */
+    abstract protected function getSupportedClasses();
 }
--- SyliusSecurityBundle/Voter/AbstractRequestConfigurationVoter.php
+++ SyliusSecurityBundle/Voter/AbstractRequestConfigurationVoter.php
@@ -10,14 +10,18 @@ namespace Kaliber5\SyliusSecurityBundle\Voter;
 
 use Kaliber5\LoggerBundle\LoggingTrait\LoggingTrait;
 use Sylius\Bundle\ResourceBundle\Controller\RequestConfiguration;
-use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
 
 /**
+ * @TODO this class was extending the AbstractVoter, removed in sf3.2, and should maybe refactored
+ *
+ *
  * Class AbstractRequestConfigurationVoter
  * @package Kaliber5\SyliusSecurityBundle\Voter
  */
-abstract class AbstractRequestConfigurationVoter extends AbstractVoter implements VoterInterface
+abstract class AbstractRequestConfigurationVoter extends Voter implements VoterInterface
 {
 
     use LoggingTrait;
@@ -26,6 +30,12 @@ abstract class AbstractRequestConfigurationVoter extends AbstractVoter implement
 
     protected $actions = array();
 
+    /**
+     * AbstractRequestConfigurationVoter constructor.
+     *
+     * @param object $resource
+     * @param array  $actions
+     */
     public function __construct($resource, array $actions = array('index', 'show', 'create', 'update', 'delete'))
     {
         $this->resource = $resource;
@@ -33,6 +43,8 @@ abstract class AbstractRequestConfigurationVoter extends AbstractVoter implement
     }
 
     /**
+     * This is from the sf2.8 AbstractVoter, to aware compatibility. removed in sf3.2, maybe this should be refactored
+     *
      * Return an array of supported classes. This will be called by supportsClass.
      *
      * @return array an array of supported classes, i.e. array('Acme\DemoBundle\Model\Product')
@@ -43,6 +55,8 @@ abstract class AbstractRequestConfigurationVoter extends AbstractVoter implement
     }
 
     /**
+     * This is from the sf2.8 AbstractVoter, to aware compatibility. removed in sf3.2, maybe this should be refactored
+     *
      * Return an array of supported attributes. This will be called by supportsAttribute.
      *
      * @return array an array of supported attributes, i.e. array('CREATE', 'READ')
@@ -64,8 +78,69 @@ abstract class AbstractRequestConfigurationVoter extends AbstractVoter implement
         if (($pos = strrpos($attribute, '.')) !== false) {
             $action = substr($attribute, ($pos+1));
             $this->logDebug('Got action: '.$action.' from '.$attribute);
+
             return $action;
         }
         throw new \InvalidArgumentException('Attribute: '.$attribute.' contains no action');
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function supports($attribute, $subject)
+    {
+        if ($this->supportsClass(get_class($subject))) {
+            return $this->supportsAttribute($attribute);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        return $this->isGranted($attribute, $subject, $token->getUser());
+    }
+
+    /**
+     * This is from the sf2.8 AbstractVoter, removed in sf3.2, maybe this should be refactored
+     */
+    protected function supportsAttribute($attribute)
+    {
+        return in_array($attribute, $this->getSupportedAttributes());
+    }
+
+    /**
+     * This is from the sf2.8 AbstractVoter, removed in sf3.2, maybe this should be refactored
+     */
+    protected function supportsClass($class)
+    {
+        foreach ($this->getSupportedClasses() as $supportedClass) {
+            if ($supportedClass === $class || is_subclass_of($class, $supportedClass)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * This is from the sf2.8 AbstractVoter, to aware compatibility. removed in sf3.2, maybe this should be refactored
+     *
+     * Perform a single access check operation on a given attribute, object and (optionally) user
+     * It is safe to assume that $attribute and $object's class pass supportsAttribute/supportsClass
+     * $user can be one of the following:
+     *   a UserInterface object (fully authenticated user)
+     *   a string               (anonymously authenticated user).
+     *
+     * @param string               $attribute
+     * @param object               $object
+     * @param UserInterface|string $user
+     *
+     * @return bool
+     */
+    abstract protected function isGranted($attribute, $object, $user = null);
+
 }
